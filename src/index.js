86e5db7ee631b19874837a930179a6793646aad8
cloudonshore <samuel.walker9@gmail.com>
2020-05-13 15:23:43 -0400

    finished ui

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import ReactDOM from "react-dom";
-import { Grommet, Box, Heading, Button } from "grommet";
+import { Grommet, Box, Heading, DataTable, Text, Paragraph } from "grommet";
 import Aggregator from "./aggregators";
 
 const aggregator = new Aggregator(1);
@@ -9,6 +9,10 @@ const aggregator = new Aggregator(1);
 const sourceToken = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 const destinationToken = "0x6b175474e89094c44da98b954eedeac495271d0f";
 const sourceAmount = "1000000000000000000";
+
+function parseWei(amount) {
+  return (Number(amount) / 10 ** 18).toFixed(4);
+}
 
 function App() {
   const [quotes, setQuotes] = useState([]);
@@ -33,7 +37,28 @@ function App() {
     >
       <Box align="center" alignContent="center" direction="column">
         <Heading>Aggregator Aggregator</Heading>
-        <Box>{JSON.stringify(quotes, null, 2)}</Box>
+        <Paragraph>Sell 1 ETH for DAI</Paragraph>
+        <DataTable
+          data={quotes}
+          columns={[
+            {
+              property: "aggregator",
+              header: "Aggregator"
+            },
+            {
+              property: "destinationAmount",
+              header: "Return Amount",
+              render: datum => (
+                <Text>{parseWei(datum.destinationAmount)} DAI</Text>
+              )
+            },
+            {
+              property: "fetchTime",
+              header: "Fetch Time (s)",
+              render: datum => <Text>{datum.fetchTime / 1000}</Text>
+            }
+          ]}
+        />
       </Box>
     </Box>
   );
